3e3459f96e736cdb0bd6315ec2a6959d64a75d12
chengfangyin2 <chengfangyin3@jd.com>
2020-01-28 10:49:34 +0800

    修改部分配置

--- src/main/java/com/chengfy/blog/service/PostService.java
+++ src/main/java/com/chengfy/blog/service/PostService.java
@@ -8,14 +8,9 @@ import com.chengfy.blog.domain.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
